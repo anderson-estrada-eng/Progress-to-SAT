f2c7b36221c8d5680903de26fd1419e3c9655f96
anderson-estrada-eng <anderpaugamer@gmail.com>
2026-09-09 11:26:26 -0500

    sync

---
+++ Day 3/poin_in_circle_validator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std; //This is so you don't have to keep mentioning `std` all the time.
+
+bool operation(double x, double h, double y, double k, double r);
+{
+    
+    bool result=((pow(x-h,2))+(pow(y-k,2)))==(pow(r,2));
+
+return;
+}
+
+$(x - h)^2 + (y - k)^2 = r^2$.
